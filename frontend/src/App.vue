ac2124bb3968ddf4189cd18a11a7786715352ce8
Takuya Kinoshita <kktak02@gmail.com>
2025-07-02 06:27:21 +0900

    adding menues in drawer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,35 @@
 <script setup lang="ts">
-import {ref, onMounted, createApp} from 'vue'
-import { supabase } from '@/lib/supabaseClient'
+import {ref, onMounted } from 'vue'
 import { useAuthStore } from '@/stores/auth'
 
 const authStore = useAuthStore()
 
-// onMounted( async () => {
-//   await authStore.initialize()
-// })
+interface MenuItem {
+  title: string,
+  icon?: string,
+  to: string,
+}
+
+const menuItems = ref<MenuItem[]>([])
+
+menuItems.value = [
+  {
+    title: 'サマリー',
+    to: '/dashboard',
+  },
+  {
+    title: '支出分析',
+    to: '/expenses',
+  },
+  {
+    title: '収入分析',
+    to: '/incomes',
+  },
+  {
+    title: '予算管理',
+    to: '/budgets',
+  }
+]
 
 onMounted( ()  =>  {
   authStore.initialize()
@@ -22,21 +44,29 @@ onMounted( ()  =>  {
       class="pt-4"
       color="grey-lighten-3"
       model-value
-      rail
     >
-      <v-avatar
-        v-for="n in 6"
-        :key="n"
-        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"
-        :size="n === 1 ? 36 : 20"
-        class="d-block text-center mx-auto mb-9"
-      ></v-avatar>
+<!--      <v-avatar-->
+<!--        v-for="n in 6"-->
+<!--        :key="n"-->
+<!--        :color="`grey-${n === 1 ? 'darken' : 'lighten'}-1`"-->
+<!--        :size="n === 1 ? 36 : 20"-->
+<!--        class="d-block text-center mx-auto mb-9"-->
+<!--      ></v-avatar>-->
+      <v-list>
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :title="item.title"
+          :to="item.to"
+        ></v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
-    <div class="d-inline pa-2">
-      <router-view></router-view>
-    </div>
-
+    <v-main>
+      <div class="d-inline pa-4">
+        <router-view></router-view>
+      </div>
+    </v-main>
   </v-app>
 </template>
 
