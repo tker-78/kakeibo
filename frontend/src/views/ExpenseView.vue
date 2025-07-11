a5416fa544aa7b0e7fdcf5a29910aefeeb4b0431
Takuya Kinoshita <kktak02@gmail.com>
2025-07-12 08:08:51 +0900

    temp fix

--- frontend/src/views/ExpenseView.vue
+++ frontend/src/views/ExpenseView.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth';
 import { useHeaderStore } from '@/stores/header';
-import { useRouter } from 'vue-router';
-import { onMounted } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 const authStore = useAuthStore();
 const headerStore = useHeaderStore();
+
 
 
 onMounted(() => {
