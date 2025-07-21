70fee3550b31bfd5d3d3b0bae355c2f5f9d97a47
Takuya Kinoshita <kktak02@gmail.com>
2025-07-22 06:56:10 +0900

    getIncomeByCategoryの切り出し

--- frontend/src/views/IncomeView.vue
+++ frontend/src/views/IncomeView.vue
@@ -26,46 +26,9 @@ const incomeData = ref<Item[]>([])
 const incomeByCategory = ref<number[]>([])
 
 
-const getIncomeListData = async () => {
-  const { data, error } = await supabase.from('incomes').select()
-  if (error) {
-    console.log('error:', error)
-    console.log('data:', data)
-    console.log('収入の取得に失敗しました。もう一度試してください。')
-  }
-  // console.log('data:', data)
-  if (data === null) {
-    incomeData.value = []
-  } else {
-    for (const item of data) {
-      if (item.income_value != null) {
-        incomeData.value.push(item)
-      }
-    }
-  }
-}
-
 const getIncomeListForThisMonth = async () => {
   incomeData.value = []
-
-  // const { data, error } = await supabase
-  //   .from('incomes')
-  //   .select('*')
-  //   .eq('user_id', authStore.user?.id)
-  //   .gte('income_month', start)
-  //   .lte('income_month', end)
-  //
-  // if (error) {
-  //   console.log('error:', error)
-  //   console.log('data:', data)
-  //   console.log('収入の取得に失敗しました。もう一度試してください。')
-  // } else {
-  //   for (const item of data) {
-  //     if (item.income_value != null) {
-  //       incomeData.value.push(item)
-  //     }
-  //   }
-  // }
+  if (!authStore.user?.id )  { return }
 
   const data = await incomeHelpers.getIncomeListForThisMonth(authStore.user.id, dateStore.date)
 
@@ -76,50 +39,15 @@ const getIncomeListForThisMonth = async () => {
       }
     }
   }
-
 }
 
 
 const getIncomeByCategory = async (date: string) => {
-  // グラフでの集計は月単位だから、7月を指定した場合は、7/1を使う。
-  // リストから情報を取得する際は、7月1日から31日までのデータを取得する。
-  const { start, end } = getMonthRange(date)
-  console.log('start:', start)
-  console.log('end:', end)
 
-  let sum_salary = 0
-  let sum_realstate = 0
-  let sum_support = 0
-  let sum_other = 0
+  if (!authStore.user?.id )  { return }
 
-  const { data, error } = await supabase
-      .from('incomes')
-      .select('*')
-      .eq('user_id', authStore.user?.id)
-      .gte('income_month', start)
-      .lte('income_month', end)
-
-  if (error) {
-    // todo
-  } else {
-    for (const item of data) {
-      if (item.category === '給与収入') {
-        sum_salary += item.income_value
-      } else if (item.category === '不動産収入') {
-        sum_realstate += item.income_value
-      } else if (item.category === '補助金収入') {
-        sum_support += item.income_value
-      } else if (item.category === 'その他収入') {
-        sum_other += item.income_value
-      }
-    }
-  }
-
-  // console.log('data:', data)
-  incomeByCategory.value = [sum_salary, sum_realstate, sum_support, sum_other]
+  incomeByCategory.value = await incomeHelpers.getIncomeByCategory(authStore.user?.id, date)
   console.log(incomeByCategory.value)
-
-
 }
 
 const fetchData = async () => {
